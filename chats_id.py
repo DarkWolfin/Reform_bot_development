5ebc512591ae6f841bfad980d1f01f79e608fcc1
Artem6771 <pecherkinaran@yandex.ru>
2023-08-22 11:40:43 +0500

    Revert "add administrator's group, add tracking and add tech_support"
    This reverts commit 8ec8863c7b935931f847f55d449042fbd49d1d64.

--- chats_id.py
+++ chats_id.py
@@ -1,3 +1,7 @@
 support_chat_id = -1001911218756
 commands_chat_id = -1001911218756
+<<<<<<< HEAD
 reports_chat_id = -1001911218756
+=======
+reports_chat_id = -1001911218756
+>>>>>>> parent of 8ec8863... add administrator's group, add tracking and add tech_support
