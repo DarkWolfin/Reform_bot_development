54a15be43f97b3b94df9c1e899509a5e9ef4968f
Artem6771 <pecherkinaran@yandex.ru>
2023-08-22 11:46:11 +0500

    new fix

--- chats_id.py
+++
@@ -1,7 +0,0 @@
-support_chat_id = -1001911218756
-commands_chat_id = -1001911218756
-<<<<<<< HEAD
-reports_chat_id = -1001911218756
-=======
-reports_chat_id = -1001911218756
->>>>>>> parent of 8ec8863... add administrator's group, add tracking and add tech_support
