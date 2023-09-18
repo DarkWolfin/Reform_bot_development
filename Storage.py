5cad15d6bb98fb67baec3f0f24da98f1055a8018
kruglov-ilya <kruglov-il1@yandex.ru>
2023-09-18 23:28:44 +0400

    feat: complete adding redis

--- Storage.py
+++ Storage.py
@@ -1,7 +1,5 @@
 from aiogram.contrib.fsm_storage.redis import RedisStorage2
 
 storage = RedisStorage2(
-  host='redis-11445.c10.us-east-1-3.ec2.cloud.redislabs.com',
-  db=0,
-  port=11445,
-  password='4yzGebP1RrtnVUzUtSevaYAG8SHTki3J')
+  host='redis',
+  port=6379)
