af591924a63d7468398082c7866ed6fa64cb0132
Artem Pecherkin <93091805+DarkWolfin@users.noreply.github.com>
2023-09-22 12:13:25 +0500

    Update chats_id.py

--- chats_id.py
+++ chats_id.py
@@ -1,5 +1,5 @@
-support_chat_id = -1001911218756
-commands_chat_id = -1001911218756
-reports_chat_id = -1001911218756
+support_chat_id = -4016990448
+commands_chat_id = -4016990448
+reports_chat_id = -4016990448
 
 recipients = ['qualitypecherkin@gmail.com', 'reform.yourself.bot@gmail.com']
