431c3db52c549df1c4708143ca80648272b5df62
Artem6771 <pecherkinaran@yandex.ru>
2023-09-23 19:07:34 +0500

    fix

--- Database.py
+++ Database.py
@@ -228,10 +228,10 @@ async def pre_quiz_workload(user_id):
     user = cur_quiz_workload.execute(
         "SELECT 1 FROM workload WHERE user_id == '{key}'".format(key=user_id)).fetchone()
     if not user:
-        timeNow = datetime.now()
-        timeNow = str(timeNow)[:7]
+        # timeNow = datetime.now()
+        # timeNow = str(timeNow)[:7]
         cur_quiz_workload.execute("INSERT INTO workload VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)",
-                         (str(user_id), str(cur_data.execute('SELECT username FROM profile WHERE user_id = ?', (user_id,)).fetchone()[0]), str(cur_data.execute('SELECT token FROM profile WHERE user_id = ?', (user_id,)).fetchone()[0]), timeNow, '', '', '', '', '', '', '', '', '', '', '', '', ''))
+                         (str(user_id), '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', ''))
         db_quiz_workload.commit()
 
 
