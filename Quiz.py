6ac9085746bcf1fb406f72d14c7415b2f1886434
Artem Pecherkin <93091805+DarkWolfin@users.noreply.github.com>
2023-09-23 20:33:14 +0500

    Update Quiz.py

--- Quiz.py
+++ Quiz.py
@@ -140,6 +140,7 @@ async def high_workload_5(message:types.Message, state:FSMContext):
     await bot.send_message(message.from_user.id, '6) Как бы вы оценили свое общее эмоциональное состояние на данный момент по шкале 1 до 10?', parse_mode='html',
                            reply_markup=ReplyKeyboardMarkup(resize_keyboard=True, row_width=5).add(KeyboardButton('1'), KeyboardButton('2'), KeyboardButton('3'), KeyboardButton('4'), KeyboardButton('5'), KeyboardButton('6'), KeyboardButton('7'), KeyboardButton('8'), KeyboardButton('9'), KeyboardButton('10')))
     await state.set_state(FSM_classes.Quiz.high_workload_6)
+    db_quiz_workload.commit()
 
 
 async def high_workload_6(message:types.Message, state:FSMContext):
